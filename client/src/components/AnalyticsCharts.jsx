852c369dda09e547bcff4fac926159ad9516be71
Thilak <thilakpec2004@gmail.com>
2026-06-05 09:47:04 +0530

    Create analytics charts component

---
+++ client/src/components/AnalyticsCharts.jsx
@@ -0,0 +1,17 @@
+function AnalyticsCharts ({transactions}) {
+
+    return (
+
+        <div className="analytics-section">
+            <h2>
+                Analytics
+            </h2>
+
+            <p>
+                Charts coming soon ... 
+            </p>
+        </div>
+    ); 
+}
+
+export default AnalyticsCharts;
